60af71a89fbbea59682986c7f36aa3cea808078b
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2023-04-01 18:05:29 -0300

    terminando o switchBnt

---
+++ trote/main.js
@@ -0,0 +1,14 @@
+var switchBtn = document.getElementById('botao');
+
+switchBtn.addEventListener('click', event => {
+    event.preventDefault();
+    var html = document.documentElement
+    var logo = document.getElementById('logo')
+    if (html.classList.contains("dark")){
+        html.classList.remove("dark")
+        logo.src = '/trote/assets/logotrote.png'
+    } else {
+        html.classList.add("dark")
+        logo.src = '/trote/assets/logotrote_branco.png'
+    }
+})
