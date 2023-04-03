2bc9f01877afc0593b1517551bf6000a76c61019
Cleyton Souza Martins <122110138+CleytonSM@users.noreply.github.com>
2023-04-02 21:38:44 -0300

    arrumando o botao menor

--- trote/main.js
+++ trote/main.js
@@ -12,3 +12,33 @@ switchBtn.addEventListener('click', event => {
         logo.src = '/trote/assets/logotrote_branco.png'
     }
 })
+
+
+const btnCalcular = document.getElementById('calcular')
+btnCalcular.addEventListener('click', event => {
+    event.preventDefault();
+    const kits = document.getElementById('kits').value
+    let pontos
+    if(kits >= 80){
+        pontos = 5000
+    }
+    else if((kits >= 64) && (kits < 80)){
+        pontos = 4000
+    }
+    else if((kits >= 40) && (kits < 64)){
+        pontos = 2500
+    }
+    else if((kits >= 16) && (kits < 40)){
+        pontos = 1000
+    }
+    else if((kits >= 0) && (kits < 16)){
+        pontos = 0
+    }
+    if (kits <= -1){
+        alert('Impossível calcular')
+    }
+    else {
+        document.getElementById('mostrar').innerHTML = `A pontuação da sua equipe foi de ${pontos} pontos`
+    }
+    
+})
